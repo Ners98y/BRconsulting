44cd2dae5777ad781cf0a92115ded73d39775604
Ners98y <91689624+Ners98y@users.noreply.github.com>
2022-01-05 15:07:22 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const navbar = document.querySelector('.nav__navbar');
 const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__navbar-item');
 
-const handleNav = () =>{
-    navbar.classList.toggle('nav__navbar--active')
+const handleNav = () => {
+	navbar.classList.toggle('nav__navbar--active');
+
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			navbar.classList.remove('nav__navbar--active');
+		});
+	});
 };
 
-navBtn.addEventListener('click', handleNav)
+navBtn.addEventListener('click', handleNav);
